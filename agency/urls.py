cfc190dab14e64ea7333550ea5783ecddb8d0eea
Andriy_Shumskyy <andshumskyy@gmail.com>
2024-01-13 22:11:17 +0200

    created forms for creating models objects in service, updated templated, urls and views

--- agency/urls.py
+++ agency/urls.py
@@ -7,17 +7,20 @@ from agency.views import (
     NewspaperListView,
     NewspaperDetailView,
     TopicListView,
-    TopicDetailView
+    TopicDetailView, RedactorCreateView, NewspaperCreateView, TopicCreateView
 )
 
 urlpatterns = [
     path("", index, name="index"),
     path("redactors/", RedactorListView.as_view(), name="redactor-list"),
     path("redactors/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"),
+    path("redactors/create/", RedactorCreateView.as_view(), name="redactor-create"),
     path("newspapers/", NewspaperListView.as_view(), name="newspaper-list"),
     path("newspapers/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail"),
+    path("newspapers/create/", NewspaperCreateView.as_view(), name="newspaper-create"),
     path("topics/", TopicListView.as_view(), name="topic-list"),
-    path("topics/<int:pk>/", TopicDetailView.as_view(), name="topic-detail")
+    path("topics/<int:pk>/", TopicDetailView.as_view(), name="topic-detail"),
+    path("topics/create/", TopicCreateView.as_view(), name="topic-create"),
 ]
 
 app_name = "agency"
--- agency/views.py
+++ agency/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -28,6 +29,13 @@ class RedactorDetailView(LoginRequiredMixin, generic.DetailView):
     model = Redactor
 
 
+class RedactorCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Redactor
+    fields = ("username", "first_name", "last_name", "password", "years_of_experiments",)
+    success_url = reverse_lazy("agency:redactor-list")
+    template_name = "agency/redactor_form.html"
+
+
 class NewspaperListView(LoginRequiredMixin, generic.ListView):
     model = Newspaper
     template_name = "agency/newspaper_list.html"
@@ -39,6 +47,13 @@ class NewspaperDetailView(LoginRequiredMixin, generic.DetailView):
     model = Newspaper
 
 
+class NewspaperCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Newspaper
+    fields = "__all__"
+    success_url = reverse_lazy("agency:newspaper-list")
+    template_name = "agency/newspaper_form.html"
+
+
 class TopicListView(LoginRequiredMixin, generic.ListView):
     model = Topic
     template_name = "agency/topic_list.html"
@@ -48,4 +63,11 @@ class TopicListView(LoginRequiredMixin, generic.ListView):
 
 class TopicDetailView(LoginRequiredMixin, generic.DetailView):
     model = Topic
+
+
+class TopicCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("agency:topic-list")
+    template_name = "agency/topic_form.html"
 
