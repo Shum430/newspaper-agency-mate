5613b84eba55eb24437c8d68714681a740674466
Andriy_Shumskyy <andshumskyy@gmail.com>
2024-01-13 14:23:57 +0200

    created view for home page and updated path in projects urls( included app urls)

--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from agency.models import Redactor, Topic, Newspaper
 
-# Create your views here.
+
+def index(request):
+    redactors = Redactor.objects.all()
+    topics = Topic.objects.all()
+    newspapers = Newspaper.objects.all()
+    context = {
+        "redactors": redactors,
+        "newspapers": newspapers,
+        "topics": topics
+    }
+    return render(request, "agency/index.html", context=context)
