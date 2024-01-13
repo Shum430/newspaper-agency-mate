6e34904e378e594622776a40fe1d334067e5a9af
Andriy_Shumskyy <andshumskyy@gmail.com>
2024-01-13 13:34:49 +0200

    register models in admin.py(customized creation and view of admin panel), made migrations and created superuser

--- agency/admin.py
+++ agency/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from agency.models import Redactor, Topic, Newspaper
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ["name",]
+    list_filter = ["name",]
+    search_fields = ["name",]
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experiments",)
+    fieldsets = UserAdmin.fieldsets + (("Important data", {"fields": ("years_of_experiments",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((
+                                                   "Important data", {
+                                                       "fields": (
+                                                           "years_of_experiments",
+                                                           "first_name",
+                                                           "last_name",
+                                                       )
+                                                   }
+                                               ),)
+
+
+@admin.register(Newspaper)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ["title", "topic", "published_date",]
+    list_filter = ["published_date",]
+    search_fields = ["name",]
