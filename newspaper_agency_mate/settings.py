172d4c8ee6abf3250eff35978438f4b3924e7353
Andriy_Shumskyy <andshumskyy@gmail.com>
2024-01-16 17:04:11 +0200

    rewrited view from func- to class -based, refactored imports in urls and changed secret_key in the settings

--- newspaper_agency_mate/settings.py
+++ newspaper_agency_mate/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h2@asy3hl--$p-uardcy4h#wg&o!c0f4g77)=s__q@xv&y8nqz'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-h2@asy3hl--$p-uardcy4h#wg&o!c0f4g77)=s__q@xv&y8nqz")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
